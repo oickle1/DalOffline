678d243504a9b16f701ffc01f7f434326d407897
Braden <Braden@Bradens_PC>
2017-03-30 15:36:33 -0300

    Making a new branch because the other one is acting up when merging

--- src/main/java/com/csci3130/daloffline/domain/Student.java
+++ src/main/java/com/csci3130/daloffline/domain/Student.java
@@ -1,14 +1,16 @@
 package com.csci3130.daloffline.domain;
 import java.io.Serializable;
+
 import java.util.ArrayList;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+
 @Entity(name = "STUDENTS")
 public class Student extends User implements Serializable{
 private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue
 	private long id;
